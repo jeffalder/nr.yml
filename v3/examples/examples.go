93a1a59fd8d16367f5dcbb7b3d9d7d3a17472690
Jeff Alder <49817386+jeffalder@users.noreply.github.com>
2020-07-03 15:12:40 -0700

    Add license header

--- v3/examples/examples.go
+++ v3/examples/examples.go
@@ -1,3 +1,16 @@
+//   Copyright 2020, Jeff Alder
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
 package main
 
 import (
@@ -13,11 +26,11 @@ import (
 // This is because newrelic.NewApplication sets values via last-wins.
 //
 //noinspection GoUnusedFunction,GoUnusedVariable
-func unusedUntestableExample() {
-	app, err := newrelic.NewApplication(
-		nr_yml.ConfigFromYamlFile("config.yml"),
-		newrelic.ConfigFromEnvironment())
-}
+//func unusedUntestableExample() {
+//	app, err := newrelic.NewApplication(
+//		nr_yml.ConfigFromYamlFile("config.yml"),
+//		newrelic.ConfigFromEnvironment())
+//}
 
 func main() {
 	_, filename, _, _ := runtime.Caller(0)
@@ -27,7 +40,9 @@ func main() {
 	cfg := new(newrelic.Config)
 
 	nr_yml.ConfigFromYamlFile(cfgFile)(cfg)
-
+	fmt.Println("--- start raw print ---")
+	fmt.Println(cfg)
+	fmt.Println("--- end raw print ---")
 	fmt.Println("app name:", cfg.AppName)
 	fmt.Println("dt enabled:", cfg.DistributedTracer.Enabled)
 	fmt.Println("policy token:", cfg.SecurityPoliciesToken)
--- v3/nr_yml/config_from_yaml_test.go
+++ v3/nr_yml/config_from_yaml_test.go
@@ -1,7 +1,19 @@
+//   Copyright 2020, Jeff Alder
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
 package nr_yml
 
 import (
-	"fmt"
 	"github.com/newrelic/go-agent/v3/newrelic"
 	"github.com/stretchr/testify/assert"
 	"io/ioutil"
@@ -93,10 +105,7 @@ production:
 	wd, _ := os.Getwd()
 	assert.NoError(t, os.Chdir(tempDir))
 	defer os.Chdir(wd)
-
-	wd2, _ := os.Getwd()
-	fmt.Println("new working dir:", wd2)
-
+	
 	cfg := new(newrelic.Config)
 	ConfigFromDefaultYaml()(cfg)
 	assert.NoError(t, cfg.Error)
